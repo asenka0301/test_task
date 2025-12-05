ac6dded026f9f19eb73fdd301c2b6373623d716b
asenka0301 <asenka0301@gmail.com>
2025-12-05 17:40:08 +0300

    add task data

---
+++ src/config.js
@@ -0,0 +1,92 @@
+import img1 from "./assets/images/cards/image_1.png";
+import img2 from "./assets/images/cards/image_2.png";
+import img3 from "./assets/images/cards/image_3.png";
+import img4 from "./assets/images/cards/image_4.png";
+import img5 from "./assets/images/cards/image_5.png";
+import img6 from "./assets/images/cards/image_6.png";
+import img7 from "./assets/images/cards/image_7.png";
+import img8 from "./assets/images/cards/image_8.png";
+import img9 from "./assets/images/cards/image_9.png";
+
+export const COURSES_NAV_ITEMS = [
+  "Marketing",
+  "Management",
+  "HR & Recruiting",
+  "Design",
+  "Development",
+];
+
+export const COURSES = [
+  {
+    id: 1,
+    title: "The Ultimate Google Ads Training Course",
+    category: "Marketing",
+    author: "Jerome Bell",
+    price: "$100",
+    image: img1,
+  },
+  {
+    id: 2,
+    title: "Product Management Fundamentals",
+    category: "Management",
+    author: "Marvin McKinney",
+    price: "$480",
+    image: img2,
+  },
+  {
+    id: 3,
+    title: "HR Management and Analytics",
+    category: "HR & Recruiting",
+    author: "Leslie Alexander Li",
+    price: "$200",
+    image: img3,
+  },
+  {
+    id: 4,
+    title: "Brand Management & PR Communications",
+    category: "Marketing",
+    author: "Kristin Watson",
+    price: "$530",
+    image: img4,
+  },
+  {
+    id: 5,
+    title: "Graphic Design Basic",
+    category: "Design",
+    author: "Guy Hawkins",
+    price: "$500",
+    image: img5,
+  },
+  {
+    id: 6,
+    title: "Business Development Management",
+    category: "Management",
+    author: "Dianne Russell",
+    price: "$400",
+    image: img6,
+  },
+  {
+    id: 7,
+    title: "Highload Software Architecture",
+    category: "Development",
+    author: "Brooklyn Simmons",
+    price: "$600",
+    image: img7,
+  },
+  {
+    id: 8,
+    title: "Human Resources – Selection and Recruitment",
+    category: "HR & Recruiting",
+    author: "Kathryn Murphy",
+    price: "$150",
+    image: img8,
+  },
+  {
+    id: 9,
+    title: "User Experience. Human-centered Design",
+    category: "Design",
+    author: "Cody Fisher",
+    price: "$240",
+    image: img9,
+  },
+];
